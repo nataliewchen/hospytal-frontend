db0c72545713227c8ca5fa65276a9b65b188592a
Natalie Chen <natalie.chen014@gmail.com>
2022-05-05 14:07:42 -0700

    change to messages framework

--- src/components/CustomAlert.js
+++ src/components/CustomAlert.js
@@ -12,10 +12,16 @@ const CustomAlert = () => {
   const getAlert = async() => {
     const response = await axios.get(`${apiUrl}alert/`, { withCredentials: true });
     console.log('alert', response.data);
+    let type = '';
+    if (response.data.type === 25) {
+      type = 'success';
+    } else if (response.data.type === 40) {
+      type = 'error';
+    }
     setAlert({
       active: true,
-      type: response.data.success ? 'success' : 'error',
-      text: response.data.success || response.data.error
+      type: type,
+      text: response.data.text ? response.data.text : ''
     })
   }
 
